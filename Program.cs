912cb1a07b754e8deebe642a3ab0b4e9ddc5500c
Ron Enubs <ronenubs@gmail.com>
2020-11-04 11:21:52 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IteratorExample3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DaysOfWeek days = new DaysOfWeek();
+            foreach(string day in days)
+            {
+                Console.Write(day + " ");
+            }
+
+            Console.ReadLine();
+        }
+
+        private class DaysOfWeek : IEnumerable
+        {
+            private string[] days =
+                {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+            public IEnumerator GetEnumerator()
+            {
+                for(int index = 0; index < days.Length; index++)
+                {
+                    yield return days[index];
+                }
+            }
+        }
+    }
+}
